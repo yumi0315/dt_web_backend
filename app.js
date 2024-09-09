afceb18a4198fae1870d4a1289eb944756747e01
yumi0315 <hym40757413@gmail.com>
2024-09-09 21:26:51 +0900

    add chart 1 & 2

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3030;
 
 app.use(express.json()); // For JSON payloads
 app.use(express.urlencoded({ extended: true })); // For URL-encoded payloads
+
+app.use(
+  cors({
+    origin: "*", // 모든 출처 허용 옵션. true를 써도 된다.
+  })
+);
 
 app.use("/", require("./routes/loginRoutes"));
 app.use("/api", require("./routes/apiRoutes"));
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authenticateJWT = require("../middleware/authMiddleware");
 
-const { getImage } = require("../controllers/apiController");
+const {
+  getImage,
+  getChart1,
+  getChart2,
+} = require("../controllers/apiController");
 
-router.use(authenticateJWT);
+// router.use(authenticateJWT);
 router.route("/image").get(getImage);
+router.route("/chart1/:proj").get(getChart1);
+router.route("/chart2").get(getChart2);
 
 module.exports = router;
