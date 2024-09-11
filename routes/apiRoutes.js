a1b1d0e07dc4cd2fe430b3780a3f74a042b73359
yumi0315 <hym40757413@gmail.com>
2024-09-11 16:05:07 +0900

    add page 3

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,11 +10,16 @@ const {
   getChart5,
 } = require("../controllers/apiController");
 
+const { getTable4, getChart4 } = require("../controllers/page3Controller");
+
 // router.use(authenticateJWT);
 router.route("/image").get(getImage);
 router.route("/chart1/:proj").get(getChart1);
 router.route("/chart2").get(getChart2);
 router.route("/chart3").get(getChart3);
 router.route("/chart5").get(getChart5);
+
+router.route("/page3/table").post(getTable4);
+router.route("/page3/chart").post(getChart4);
 
 module.exports = router;
