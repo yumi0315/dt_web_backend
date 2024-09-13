a476b52de0c8ed0fe53275c6a4304edd5319cb5c
yumi0315 <hym40757413@gmail.com>
2024-09-13 10:42:11 +0900

    page5 add chart2 table2,3

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,6 +13,12 @@ const {
 
 const { getTable4, getChart4 } = require("../controllers/page3Controller");
 
+const {
+  page5table2,
+  page5chart2,
+  page5table3,
+} = require("../controllers/page5Controller");
+
 // router.use(authenticateJWT);
 router.route("/image").get(getImage);
 router.route("/chart1/:proj").get(getChart1);
@@ -23,5 +29,9 @@ router.route("/chart5").get(getChart5);
 
 router.route("/page3/table").post(getTable4);
 router.route("/page3/chart").post(getChart4);
+
+router.route("/page5/table2").get(page5table2);
+router.route("/page5/chart2").get(page5chart2);
+router.route("/page5/table3").get(page5table3);
 
 module.exports = router;
